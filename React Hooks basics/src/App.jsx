ab30511a3e9731505648f7ed02d8c9b340d01a59
athithyaramaa1 <athithya.ramma@kalvium.community>
2024-02-23 18:04:24 +0530

    Prog6

--- React Hooks basics/src/App.jsx
+++ React Hooks basics/src/App.jsx
@@ -1,8 +1,9 @@
 import './App.css'
-import Prog3 from './Progressions/Prog3'
-import UseState2 from './Progressions/Prog2'
-import UseState4 from './Progressions/Prog4'
-import UseState5 from './Progressions/Prog5'
+import Prog6 from './Progressions/Prog6'
+// import Prog3 from './Progressions/Prog3'
+// import UseState2 from './Progressions/Prog2'
+// import UseState4 from './Progressions/Prog4'
+// import UseState5 from './Progressions/Prog5'
 // import UseState from './Progressions/UseState'
 
 function App() {
@@ -13,7 +14,9 @@ function App() {
       {/* <UseState2 /> */}
       {/* <Prog3 /> */}
       {/* <UseState4 /> */}
-      <UseState5/>
+      {/* <UseState5/> */}
+      <Prog6 />
+
     </>
   )
 }
---
+++ React Hooks basics/src/Progressions/prog7.jsx
@@ -0,0 +1,9 @@
+const prog7 = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default prog7
